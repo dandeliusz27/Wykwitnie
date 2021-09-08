32ef3199998a8135b494b8ab2458dd2ee2bbc0c1
Duli <dandeliusz27@gmail.com>
2021-09-08 16:12:15 +0200

    added detailed products sites

--- src/components/pages/Products/Product1.js
+++ src/components/pages/Products/Product1.js
@@ -7,7 +7,7 @@ function Product1() {
     return (
         <DetailItem 
             title="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, sequi?"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. A ex pariatur quibusdam cupiditate, esse laudantium impedit enim praesentium cum ab mollitia hic provident. Eos natus officia animi et quam voluptatum at molestias voluptate nulla quo magnam sequi facere suscipit laudantium labore exercitationem rem optio, aut nam ipsam asperiores ducimus praesentium! Impedit eius est, odit doloremque, sequi neque, autem provident corporis officiis cum incidunt consequuntur sint totam nisi ea. Repellat quis rem neque veniam culpa. Totam qui eos officiis cupiditate accusamus non magni rem nemo, mollitia dolorem, repellat cumque necessitatibus? Debitis sequi non quas quasi labore necessitatibus esse sint repellat odio!"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. A ex pariatur quibusdam cupiditate, esse laudantium impedit enim praesentium cum ab mollitia hic provident. Eos natus officia animi et quam voluptatum at molestias voluptate nulla quo magnam sequi facere suscipit laudantium labore exercitationem rem optio, "
             src={Rose}
         />
     )
